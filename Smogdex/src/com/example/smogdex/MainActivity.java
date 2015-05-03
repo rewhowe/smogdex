10c38e0c25ff1ddff3116e4dd844ed2985a0c04f
Rew Howe <rewhowe@gmail.com>
2015-05-03 14:56:39 -0400

    added pokemon names and aliases, made an accessor which will turn into our list manager later

--- Smogdex/src/com/example/smogdex/MainActivity.java
+++ Smogdex/src/com/example/smogdex/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.smogdex;
 
-import java.util.Map;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,41 +10,38 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    
-    @Override
-    protected void onStart() {
-    	super.onStart();
-    	ImageView img = (ImageView) findViewById(R.id.temp_image);
-    	TextView txt = (TextView) findViewById(R.id.temp_text);
-    	
-    	for (Map.Entry<Integer, Integer> entry : PokemonConstants.POKEMON_LIST.entrySet()) {
-    		img.setImageResource(entry.getKey());
-    		txt.setText(getText(entry.getValue()));
-    	}
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		ImageView img = (ImageView) findViewById(R.id.temp_image);
+		TextView txt = (TextView) findViewById(R.id.temp_text);
+		img.setImageResource(R.drawable.p109);
+		txt.setText(getText(R.string.p109));
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
